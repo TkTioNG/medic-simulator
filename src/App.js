e40cb50ac49d2a7b549d8bc99322982cb2769dc1
TkTioNG <tktiong@icloud.com>
2020-11-09 17:46:47 +0800

    Start Project

--- src/App.js
+++ src/App.js
@@ -1,34 +1,47 @@
-import React, { useEffect } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 
-import * as actions from './actions';
-import soldierStatusEnum from './enums/soldierStateEnum'
-import soldierLogo from './soldier.png';
-import injuredSoldierLogo from './injuredSoldier1.jpg'
-import './App.css';
-
+import * as actions from "./actions";
+import soldierStatusEnum from "./enums/soldierStateEnum";
+import soldierLogo from "./images/soldier.png";
+import injuredSoldierLogo from "./images/injuredSoldier1.jpg";
+import "./App.css";
 
 const App = (props) => {
   const { soldiers, prepareBattlefield, startBattle } = props;
 
   useEffect(() => {
     prepareBattlefield();
-  }, [])
-  
+  }, [prepareBattlefield]);
+
   const handleClick = (e) => {
-    startBattle()
-  }
+    startBattle();
+  };
+
   return (
     <div className="App">
       <button onClick={handleClick}> Start Battle!</button>
-      {soldiers && Object.values(soldiers).map(soldier => 
-          <img alt="soldierLogo" className="soldierLogo" src={soldier.status === soldierStatusEnum.HEALTHY ? soldierLogo : injuredSoldierLogo}
-          style={{ position: 'absolute', top: soldier.coordinates.y*10, left: soldier.coordinates.x*15 }}/>
-        )}
+      {soldiers &&
+        Object.values(soldiers).map((soldier) => (
+          <img
+            alt="soldierLogo"
+            className="soldierLogo"
+            src={
+              soldier.status === soldierStatusEnum.HEALTHY
+                ? soldierLogo
+                : injuredSoldierLogo
+            }
+            style={{
+              position: "absolute",
+              top: soldier.coordinates.y * 10,
+              left: soldier.coordinates.x * 15,
+            }}
+          />
+        ))}
     </div>
   );
-}
+};
 
 function mapStateToProps(state) {
   return {
@@ -37,10 +50,13 @@ function mapStateToProps(state) {
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({
-    prepareBattlefield: actions.prepareBattlefield,
-    startBattle: actions.startBattle,
-  }, dispatch)
+  return bindActionCreators(
+    {
+      prepareBattlefield: actions.prepareBattlefield,
+      startBattle: actions.startBattle,
+    },
+    dispatch
+  );
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/reducers/soldier.js
+++ src/reducers/soldier.js
@@ -1,27 +1,25 @@
-
-import * as utils from '../utils';
-import * as actions from '../actions';
+import * as utils from "../utils";
+import * as actions from "../actions";
 
 export default function soldierReducer(state = {}, action) {
-    switch (action.type) {
-        case actions.PREPARE_BATTLEFIELD: {
-            const initialLocations = utils.initializeBattlefield();
-            return Object.freeze({
-                ...state,
-                ...initialLocations
-                
-            })
-        }
-        case actions.CYCLE: {
-            const newSoldiersState = utils.getNewSoldiersState(state);
-            return Object.freeze({
-                ...state,
-                ...newSoldiersState
-            })
-        }
-        // Add other Actions here
-        default: {
-            return state;
-        }
+  switch (action.type) {
+    case actions.PREPARE_BATTLEFIELD: {
+      const initialLocations = utils.initializeBattlefield();
+      return Object.freeze({
+        ...state,
+        ...initialLocations,
+      });
     }
+    case actions.CYCLE: {
+      const newSoldiersState = utils.getNewSoldiersState(state);
+      return Object.freeze({
+        ...state,
+        ...newSoldiersState,
+      });
+    }
+    // Add other Actions here
+    default: {
+      return state;
+    }
+  }
 }
